cd9f48c11c95938c196cbba913f5b1acd815be97
Dan Iverson <dan@daniverson.com>
2020-04-10 08:31:58 -0500

    Start Windows pre script

---
+++ win/pre.ps1
@@ -0,0 +1,57 @@
+#Requires -Version 5
+
+<#PSScriptInfo
+    .VERSION 1.0
+    .AUTHOR psadmin.io
+    .SYNOPSIS
+        Cloud Manager pre-provisioning script for Windows
+    .DESCRIPTION
+        Installs utities for Windows PeopleSoft instances
+    # .PARAMETER PATCH_ID
+    #     Patch ID for the PUM image
+    .EXAMPLE
+        pre.ps1
+
+#>
+
+#-----------------------------------------------------------[Parameters]----------------------------------------------------------
+
+[CmdletBinding()]
+Param(
+)
+
+
+#---------------------------------------------------------[Initialization]--------------------------------------------------------
+
+# Valid values: "Stop", "Inquire", "Continue", "Suspend", "SilentlyContinue"
+$ErrorActionPreference = "Stop"
+$DebugPreference = "SilentlyContinue"
+$VerbosePreference = "SilentlyContinue"
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+#------------------------------------------------------------[Variables]----------------------------------------------------------
+
+
+
+#-----------------------------------------------------------[Functions]-----------------------------------------------------------
+
+function install_choco() {
+  Set-ExecutionPolicy Bypass -Scope Process -Force; 
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+function install_utilities() {
+  choco install vscode -y
+  choco install firefix -y
+  choco install grep -y
+  choco install git -y
+}
+
+#-----------------------------------------------------------[Execution]-----------------------------------------------------------
+
+##########
+#  Main  #
+##########
+
+. install_choco
+. install_utilities
